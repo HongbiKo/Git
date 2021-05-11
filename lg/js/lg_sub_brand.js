95bde74678ec18b99cce881507289bd3f5d4fa8a
HongbiKo <83524159+HongbiKo@users.noreply.github.com>
2021-05-11 12:18:28 +0900

    0511 brand mobile css changed

--- lg/js/lg_sub_brand.js
+++ lg/js/lg_sub_brand.js
@@ -3,18 +3,17 @@ $(document).ready(function(){
     $(".tab ul li").click(function(){
         $(this).addClass('on').siblings().removeClass('on');
         $("#"+$(this).data('id')).addClass('on').siblings().removeClass('on');
-
-        $("#"+$(this).data('id')).find('.swiper-container')[0].swiper.update();
-        $("#"+$(this).data('id')).find('.swiper-container')[1].swiper.update();
-        $("#"+$(this).data('id')).find('.swiper-container')[2].swiper.update();
-        $("#"+$(this).data('id')).find('.swiper-container')[3].swiper.update();
-        $("#"+$(this).data('id')).find('.swiper-container')[4].swiper.update();
-        $("#"+$(this).data('id')).find('.swiper-container')[5].swiper.update();
-        $("#"+$(this).data('id')).find('.swiper-container')[6].swiper.update();
-        $("#"+$(this).data('id')).find('.swiper-container')[7].swiper.update();
-
-        
     });
+    // $(".tab ul li:eq(1)").click(function(){
+    //     $("#"+$(this).data('id')).find('.swiper-container')[0].swiper.update();
+    //     $("#"+$(this).data('id')).find('.swiper-container')[1].swiper.update();
+    //     $("#"+$(this).data('id')).find('.swiper-container')[2].swiper.update();
+    //     $("#"+$(this).data('id')).find('.swiper-container')[3].swiper.update();
+    //     $("#"+$(this).data('id')).find('.swiper-container')[4].swiper.update();
+    //     $("#"+$(this).data('id')).find('.swiper-container')[5].swiper.update();
+    //     $("#"+$(this).data('id')).find('.swiper-container')[6].swiper.update();
+    //     $("#"+$(this).data('id')).find('.swiper-container')[7].swiper.update();
+    // });
 
 
     //pc box hover
@@ -28,16 +27,20 @@ $(document).ready(function(){
             $(this).children('p').removeClass('on');
         })
     }
+
 });
 
 var swiper1 = new Swiper(".mySwiper.Subswiper", {
     centeredSlides: true,
-    slidesPerView: 1,
+    slidesPerView: 2,
             spaceBetween: 0,
             pagination: {
             el: ".swiper-pagination",
             clickable: true,
             },
+    observer: true,
+    observeParents: true,
+    //각 탭에서 swiper업데이트 해주기(위의 배열을 쓰지 않고 쓰와이퍼 변수안에서 이걸 써주면 알아서 업데이트 32.tabcarousel참조)
     breakpoints : {
         640 : {
             centeredSlides: true,
