17cb48f59d5854777111989d36211c29de7263bb
HongbiKo <83524159+HongbiKo@users.noreply.github.com>
2021-05-24 17:28:39 +0900

    sunpictures editing 0524

--- portfolio/sunpictures/js/sunpictures.js
+++ portfolio/sunpictures/js/sunpictures.js
@@ -57,3 +57,20 @@ var swiper = new Swiper(".mySwiper.ticketSwiper", {
         clickable: true,
     },
     });
+
+
+
+
+
+// var swiper = new Swiper(".mySwiper", {
+//     speed: 600,
+//     parallax: true,
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true
+//     },
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev"
+//     }
+// });
