36830efebc32828551a0a070e16e5e1a77c00bce
HongbiKo <83524159+HongbiKo@users.noreply.github.com>
2021-05-27 12:58:36 +0900

    index work editing

--- index.js
+++ index.js
@@ -82,3 +82,16 @@ $(document).ready(function(){
     });
     
 });
+
+
+//스와이퍼
+var swiper = new Swiper(".project", {
+    pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
